c598948feca25efb48ad3508ba2baf0cd76f90a9
Safin Roman <r.n.safin@gmail.com>
2020-11-30 06:01:46 +0400

    update

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,30 @@
 package main
 
 import (
+	"exchangerate/internal/bot"
 	"exchangerate/internal/config"
 	"exchangerate/internal/exchangerate"
 	"fmt"
-	/*"github.com/go-telegram-bot-api/telegram-bot-api"
-	"fmt"*/
 	"log"
 	"time"
 )
 
-type Config struct {
-	App struct{
-		Bot struct {
-			Key string `yaml:"key"`
-		} `yaml:"bot"`
-	} `yaml:"app"`
-}
-
 func checkRate(c chan float64){
+	var oldRate float64 = 0
+
 	p := new(exchangerate.Profinance)
 	for {
 		rate, err := p.GetRate("")
 		if err != nil {
 			log.Panic(err)
 		}
-		c <- rate
-		time.Sleep(5 * time.Second)
+
+		if oldRate != rate {
+			oldRate = rate
+			c <- rate
+		}
+
+		time.Sleep(60 * time.Second)
 	}
 }
 
@@ -35,42 +33,13 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
-	fmt.Printf("%+v\n", cfg)
 
-
-	/*bot*/
-	/*bot, err := tgbotapi.NewBotAPI()
-	if err != nil {
-		log.Panic(err)
-	}
-
-	bot.Debug = true
-
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates, err := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		if update.Message == nil { // ignore any non-Message Updates
-			continue
-		}
-
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		bot.Send(msg)
-	}*/
-
-	/*rate*/
-	/*var c = make(chan float64)
+	var c = make(chan float64)
 	go checkRate(c)
+
+	go bot.CreateBot(cfg.App.Bot.Key)
 
 	for msg := range c {
 		fmt.Printf("%f\n", msg)
-	}*/
+	}
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module exchangerate
 go 1.15
 
 require (
-	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0
 )
